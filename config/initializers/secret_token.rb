67f5403737dc40f4cf47b9620b6d881b525a8662
Grace Cooper <gracecooper89@gmail.com>
2013-03-21 17:48:47 +0000

    blagger first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blaggr::Application.config.secret_token = '677b357e8637d071dc9a600d0fdf42ef633d651fccf176483a578198dd93aa4f4e2c14ff8ffffeaecb492f274b41c391e8b744a5be4280e40450777cf0c8557a'
